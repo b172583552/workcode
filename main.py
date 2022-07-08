a90638929065409fd652694596f4ca812f1f74c5
codingnoob <99176254+b172583552@users.noreply.github.com>
2022-07-08 12:34:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 
-requiredlist=[]
+requiredlist = []
 outstandinglist = []
-code = "FM"
-matchstr = "Outstanding: "
+code = input("code: ")
+matchstr = "Outstanding:"
 outstanding = 0
-currency = {"HKD": 1.0300, "RMB": 1.2537, "USD": 8.0113, "GBP": 11.0516}
-
-with open('29-10-2021.txt') as fo:
+HKD,RMB,USD,GBP = [float(i) for i in input("HKD,RMB,USD,GBP ").split()]
+currency = {"HKD": HKD, "RMB": RMB, "USD": USD, "GBP": GBP}
+textfile = input("textfile: ")
+with open(textfile) as fo:
     for rec in fo:
         if rec[0:2] == code:
             requiredlist.append(outstanding)
@@ -22,28 +23,33 @@ with open('29-10-2021.txt') as fo:
             if k in rec:
                 curr_currency = currency[k]
         if matchstr in rec:
-            temp = rec[13:]
+            temp = ""
+            for char in reversed(rec[0:rec.find(matchstr)]):
+                temp += char
+                if char == " ":
+                    break
+            temp = temp[::-1]
             temp = temp.replace(",", "")
             temp = temp.strip("\n")
+            temp = temp.strip()
             outstanding += (float(temp) * curr_currency)
     requiredlist.append(outstanding)
 del requiredlist[0]
+
+
+
+
 # Open a file
-acc = open("acc.txt", "w")
+acc = open("account.txt", "w")
 outstandingfile = open("outstanding.txt", "w")
 for i in range(len(requiredlist)):
-    if i%2 == 1:
+    if i % 2 == 1:
         print(requiredlist[i])
         outstandingfile.write(str(requiredlist[i])+"\n")
     else:
         print(requiredlist[i])
         acc.write(str(requiredlist[i])+"\n")
 # Close opend file
-
-
-
-
-
 
 
 
